be496e96de978ff7392ead7d920cc35e51a8901b
lessthantrue <nickm@outofthisworld.net>
2016-01-01 18:43:12 -0800

    Initial Commit
    Everything but [LANG]RoboticsLib.Drive and TimeoutInvokeExtensions

---
+++ JavaRoboticsLib/WPIExtensions/ManagedCompressor.java
@@ -0,0 +1,90 @@
+package JavaRoboticsLib.WPIExtensions;
+
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.ControllerPower;
+
+/**
+* A Compressor which will not run if the voltage is too low.
+*/
+public class ManagedCompressor  extends Compressor 
+{
+    //private Notifier m_periodic;
+    private boolean m_usingTimer;
+    private final double m_period;
+    private double m_voltageThreshold;
+    private double m_voltageDeadband;
+    
+    /**
+    * If the {@link #ManagedCompressor} is being updated automatically and periodically
+    */
+    public void useTimer(boolean value) {
+        m_usingTimer = value;
+        if (value)
+            ;//m_periodic.StartPeriodic(m_period);
+        else
+            ;//m_periodic.Stop(); 
+    }
+
+    /**
+    * Voltage to turn off the compressor at
+    */
+    public double getVoltageThreshold() {
+        return m_voltageThreshold;
+    }
+
+    public void setVoltageThreshold(double value) {
+        m_voltageThreshold = value;
+    }
+
+    /**
+    * Deadband for the voltage on/off trigger
+    */
+    public double getVoltageDeadband() {
+        return m_voltageDeadband;
+    }
+
+    public void setVoltageDeadband(double value) {
+        m_voltageDeadband = value;
+    }
+
+    /**
+    * Creates a new Managed Compressor, which will turn off when the voltage is below the specified level.
+    * 
+    *  @param voltageThreshold Voltage to turn off the compressor at
+    */
+    public ManagedCompressor(double voltageThreshold) {
+        this(voltageThreshold, 0.1);
+    }
+
+    /**
+    * Creates a new Managed Compressor, which will turn off when the voltage is below the specified level.
+    * 
+    *  @param voltageThreshold Voltage to turn off the compressor at
+    *  @param Period time to wait between on and off values.
+    */
+    public ManagedCompressor(double voltageThreshold, double period) {
+        m_period = period;
+        setVoltageThreshold(voltageThreshold);
+        //m_periodic = new Notifier(Update);
+        useTimer(true);
+    }
+
+    /**
+    * Manually Updates the compressor, turning it on or off if necessary.
+    * Called automatically according to the period value if {@link #UseTimer} is enabled.
+    */
+    public void update() {
+        if (ControllerPower.getInputVoltage() < getVoltageThreshold() - getVoltageDeadband())
+        {
+            stop();
+        }
+        else if (ControllerPower.getInputVoltage() > getVoltageThreshold() + getVoltageDeadband())
+        {
+            start();
+        }
+          
+    }
+
+}
+
+
